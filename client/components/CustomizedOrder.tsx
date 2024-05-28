15ddf09bb61fef9c19fea7da5f1cd31026f258b3
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2024-05-28 16:31:27 +1200

    add data to customized order component

--- client/components/CustomizedOrder.tsx
+++ client/components/CustomizedOrder.tsx
@@ -3,6 +3,8 @@ import { MenuItem } from '../../types/menu'
 import { Collapse } from 'react-collapse'
 import '../styles/CustomizedOrder.css'
 
+import { useGetData } from '../../data/hooks'
+
 export default function CustomizedOrder({
   data,
   setModalStatus,
@@ -10,9 +12,24 @@ export default function CustomizedOrder({
   data: MenuItem
   setModalStatus: React.Dispatch<React.SetStateAction<boolean>>
 }) {
+  const { data: upgrades, isLoading, isError } = useGetData('upgrades')
+
+  if (isLoading) {
+    return <p>Loading ......</p>
+  }
+
+  if (isError) {
+    console.log('Error occured')
+  }
+
+  if (upgrades) {
+    console.log(upgrades)
+  }
+
   function handleSubmit() {
     setModalStatus(false)
   }
+
   return (
     <div className="flex w-[45rem]">
       <div className="w-1/4 border-r-2 border-slate-300] bg-center">
@@ -27,12 +44,35 @@ export default function CustomizedOrder({
           <h1 className="text-3xl font-semibold mb-8">{data.name as string}</h1>
           <p className="mb-8">{data.description}</p>
         </div>
-        <h2 className="text-xl font-semibold">
-          First, select your size and crust
-        </h2>
-        <Collapse isOpened={true}>
-          <div className="h-72">Random content</div>
-        </Collapse>
+        <div className="w-full p-6">
+          <h2 className="text-xl font-semibold">First, select your size</h2>
+          <Collapse isOpened={true}>
+            <div className="flex gap-4">
+              <div>
+                <input type="radio" name="size_choice" />
+                <label htmlFor="size_choice">{`Large ${data.price_large}`}</label>
+              </div>
+              <div>
+                <input type="radio" name="size_choice" />
+                <label htmlFor="size_choice">{`Small ${data.price_small}`}</label>
+              </div>
+            </div>
+          </Collapse>
+          <h2 className="text-xl font-semibold">Choose your extras</h2>
+          <Collapse isOpened={true}>
+            <div>
+              {upgrades?.map((item) => (
+                <div className="flex justify-between">
+                  <div>
+                    <input type="checkbox" name={item.name} />
+                    <label htmlFor={item.name}>{item.name}</label>
+                  </div>
+                  <p>{`+ $${item.price}`}</p>
+                </div>
+              ))}
+            </div>
+          </Collapse>
+        </div>
         <Button
           className="p-3 w-96 bg-green-500 text-white my-2"
           onClick={() => handleSubmit()}
