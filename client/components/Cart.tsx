93cfbcf67259db7a82eb43d68e13f60103e2c293
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2024-06-04 13:19:40 +1200

    added change delivery option to the side bar

--- client/components/Cart.tsx
+++ client/components/Cart.tsx
@@ -3,16 +3,57 @@ import { Collapse } from 'react-collapse'
 import { CartContext } from './CartProvider'
 import '../styles/Cart.css'
 import CartItemDetails from '../utils/CartItemDetails'
+import { getLocalStorage, setLocalStorage } from '../../data/localStorage'
 
 export default function Cart() {
   const cart = useContext(CartContext)
+  const order = getLocalStorage()
   const [window, setWindow] = useState('')
+  const [address, setAddress] = useState<string>('')
+  const [isOpen, setIsopen] = useState(false)
+  const [orderStatus, setOrderStatus] = useState(order.order)
+
+  console.log(order)
+
+  const deliveryCharges: number = orderStatus === 'Deliver' ? 5.99 : 0
 
   function showDetails(item: string): void {
     if (window === item) {
       setWindow('')
     } else {
       setWindow(item)
+    }
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setAddress(e.target.value)
+  }
+
+  function handleClick(): void {
+    const obj = {
+      address: '',
+      order: 'Pickup',
+    }
+    if (order.order === 'Deliver') {
+      setLocalStorage(obj)
+      setOrderStatus('Pickup')
+    }
+    if (order.order === 'Pickup') {
+      setIsopen(true)
+    }
+  }
+
+  function handleAddress(): void {
+    const obj = {
+      address: address,
+      order: 'Deliver',
+    }
+    if (address === '') {
+      alert('please enter an address')
+    } else {
+      setLocalStorage(obj)
+      setOrderStatus('Deliver')
+      setIsopen(false)
     }
   }
 
@@ -65,7 +106,45 @@ export default function Cart() {
           ))}
       </ul>
       {cart.cart.length > 0 && (
-        <p className="text-base mt-4">{`Total Cost: $${cart.calculateTotalCost()}`}</p>
+        <>
+          <div className="mt-4 flex justify-between">
+            {orderStatus === 'Deliver' ? (
+              <p className="text-sm">Delivery Charges: $5.99</p>
+            ) : (
+              <p className="text-sm">Pick Up</p>
+            )}
+            <button
+              onClick={handleClick}
+              className="text-sm leto-100 text-limeGreen hover:text-lime-800"
+            >
+              {`Change to ${
+                order.order === 'Deliver' ? 'Pick Up' : 'Delivery'
+              }`}
+            </button>
+          </div>
+          <Collapse isOpened={isOpen}>
+            <div className="mt-2 p-2 bg-gray-100">
+              <input
+                className={`shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-sm`}
+                id="address"
+                type="text"
+                onChange={handleChange}
+                value={address}
+                placeholder="Enter your address"
+              />
+              <button
+                onClick={handleAddress}
+                className="p-1 rounded text-white bg-limeGreen mt-2"
+              >
+                Done
+              </button>
+            </div>
+          </Collapse>
+
+          <p className="text-base mt-4">{`Total Cost: $${
+            Number(cart.calculateTotalCost()) + Number(deliveryCharges)
+          }`}</p>
+        </>
       )}
     </>
   )
